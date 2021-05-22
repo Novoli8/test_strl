6b7c6f1fd13c36d765c038fdf24d1695d66d66e2
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2021-05-22 17:36:05 +0300

    Add files via upload

---
+++ rete.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from streamlit_cropper import st_cropper
+from PIL import Image, ImageDraw, ImageFont
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# Upload an image and set some options for demo purposes
+
+img_file = st.file_uploader(label='Upload a file', type=['png', 'jpg', 'webp'])
+
+a = st.text_input('Enter some text')
+
+if img_file and a:
+    if len(a) == 6:
+        text = (a[:2] + '           2564' + '       ' +  a[2:-2] + ':' + a [4:])
+    elif len(a) == 5:
+        text = (a[:1] + '           2564' + '       ' +  a[1:-2] + ':' + a [3:])
+    else:
+        print("wrong")
+
+    img = Image.open(img_file)
+    w1 = Image.open('putches/water.png')
+    w2 = Image.open('putches/waterline2.png')
+    w3 = Image.open('putches/white.png')
+    w4 = Image.open('putches/thfn.png')
+
+    img.paste(w1,(170, 353))
+    img.paste(w2,(417, 353))
+    img.paste(w3,(268, 352))
+                
+    idraw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("putches/Inter-Bold.ttf", size=23)
+    idraw.text((236, 349), text,(0, 0, 0), font=font)
+    img.paste(w4,(269, 354))
+
+    
+    
+
+    st.image(img)
