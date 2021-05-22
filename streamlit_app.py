0b33034a5f6b0f2bdf19d7647c74d050a5ed1fcc
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2021-05-22 17:48:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_cropper import st_cropper
+#from streamlit_cropper import st_cropper
 from PIL import Image, ImageDraw, ImageFont
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
