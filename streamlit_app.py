7de0576d32992924548dc4bd4548ea7662a87468
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2021-06-14 16:16:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,12 @@ from PIL import Image, ImageDraw, ImageFont
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 img_file = st.file_uploader(label='Upload a file', type=['png', 'jpg', 'jpeg'])
+if img_file.size[1] < 1200:
+    img_file = img_file.resize(720, 1463)
+
+
 a = st.text_input('Enter some text')
+
 
 if img_file and a:
     if len(a) == 6:
