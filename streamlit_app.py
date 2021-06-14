94b8e792c46fee00b693bfe789cf693ec9694f46
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2021-06-14 16:23:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-from PIL import Image, ImageDraw, ImageFont
-st.set_option('deprecation.showfileUploaderEncoding', False)
+from PIL import Image,ImageDraw,ImageFont
 
-img_file = st.file_uploader(label='Upload a file', type=['png', 'jpg', 'jpeg']
+st.set_option('deprecation.showfileUploaderEncoding',False)
+
+img_file = st.file_uploader(label='Upload a file',type=['png','jpg','jpeg'])
 a = st.text_input('Enter some text')
-
 
 if img_file and a:
     if len(a) == 6:
-        text = (a[:2] + '           2564' + '         ' +  a[2:-2] + ':' + a [4:])
+        text = (a[:2] + '           2564' + '         ' + a[2:-2] + ':' + a[4:])
     elif len(a) == 5:
-        text = (a[:1] + '           2564' + '       ' +  a[1:-2] + ':' + a [3:])
+        text = (a[:1] + '           2564' + '       ' + a[1:-2] + ':' + a[3:])
     else:
         print("wrong")
 
@@ -20,13 +20,13 @@ if img_file and a:
     w3 = Image.open('putches/white.png')
     w4 = Image.open('putches/thfn.png')
 
-    img.paste(w1,(170, 353))
-    img.paste(w2,(417, 353))
-    img.paste(w3,(268, 352))
-                
+    img.paste(w1,(170,353))
+    img.paste(w2,(417,353))
+    img.paste(w3,(268,352))
+
     idraw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("putches/Inter-Bold.ttf", size=23)
-    idraw.text((232, 349), text,(0, 0, 0), font=font)
-    img.paste(w4,(262, 353))
-        
+    font = ImageFont.truetype("putches/Inter-Bold.ttf",size=23)
+    idraw.text((232,349),text,(0,0,0),font=font)
+    img.paste(w4,(262,353))
+
     st.image(img)
