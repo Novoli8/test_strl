b10a3fb1af7c7f121eb8ba73c821a9eebd7bc25e
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2021-09-23 10:01:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if img_file and a:
     w1 = Image.open('putches/water.png')
     w2 = Image.open('putches/waterline2.png')
     w3 = Image.open('putches/white.png')
-    w4 = Image.open('putches/thfn3.png')
+    w4 = Image.open('putches/thfn4.png')
 
     img.paste(w1,(170,353))
     img.paste(w2,(417,353))
