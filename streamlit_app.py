93f79bff37b5fe854bcf55505ace0a7f253596d0
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2022-03-20 17:42:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ if img_file and a:
     idraw = ImageDraw.Draw(img)
     font = ImageFont.truetype("putches/Inter-Bold.ttf",size=23)#23
     idraw.text((235,339),text,(0,0,0),font=font)#250,349
-    img.paste(w4,(266,354))#266,355
+    img.paste(w4,(266,344))#266,355
 
     st.image(img)
