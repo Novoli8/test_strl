50b797c21cb98fa2b3612933ca12797374a5e3c3
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2022-05-07 10:41:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,25 @@
 import streamlit as st
+import pytz
 from PIL import Image,ImageDraw,ImageFont
+from datetime import datetime
+
 
 st.set_option('deprecation.showfileUploaderEncoding',False)
-
 img_file = st.file_uploader(label='Upload a file',type=['png','jpg','jpeg','webp'])
 
 a = st.text_input('Enter some text')
 size = (720, 1463)
+tz = pytz.timezone('Asia/Bangkok')
+months = ('0',"ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค.")
+current_datetime = datetime.now(tz)
+mth = current_datetime.month
 
 if img_file and a:
-    
+
     if len(a) == 6:
-        text = (a[:2] + '            2565' + '         ' + a[2:-2] + ':' + a[4:])
+        text2 = (a[:2] +' '+ months[mth] +' 2565     ' + a[2:-2] + ':' + a[4:])
     elif len(a) == 5:
-        text = (a[:1] + '           2565' + '       ' + a[1:-2] + ':' + a[3:])
+        text2 = (a[:1] +' '+ months[mth] +' 2565     ' + a[1:-2] + ':' + a[3:])
     else:
         print("wrong")
 
@@ -24,15 +30,13 @@ if img_file and a:
     w1 = Image.open('putches/water.png')
     w2 = Image.open('putches/waterline2.png')
     w3 = Image.open('putches/white.png')
-    w4 = Image.open('putches/thfn8.jpg')
 
     img.paste(w1,(170,340))#170,340  170,356
     img.paste(w2,(417,339))#417,339  417,355
     img.paste(w3,(270,338))#268,338  268,352
 
     idraw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("putches/Inter-Bold.ttf",size=23)#23
-    idraw.text((228,349),text,(0,0,0),font=font)#250,349  , 235,336
-    img.paste(w4,(246,353))#266,355  , 269,341
+    font = ImageFont.truetype("putches/Noto Sans Thai UI Bold_new.ttf",size=22)#23
+    idraw.text((250,333),text2,(0,0,0),font=font)#250,349  , 235,336
 
     st.image(img)
