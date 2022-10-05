ec91be90aada1fd404be14097ac0cd5467f8f219
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2022-10-05 07:31:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ if img_file and a:
     w2 = Image.open('putches/waterline2.png')
     w3 = Image.open('putches/white.png')
 
-    img.paste(w1,(170,328))#170,340  170,356
-    img.paste(w2,(417,330))#417,339  417,355
-    img.paste(w3,(268,330))#268,338  268,352
+    img.paste(w1,(170,324))#170,340  170,356
+    img.paste(w2,(417,326))#417,339  417,355
+    img.paste(w3,(268,326))#268,338  268,352
 
     idraw = ImageDraw.Draw(img)
     font = ImageFont.truetype("putches/Noto Sans Thai UI Bold_new.ttf",size=23)#23
