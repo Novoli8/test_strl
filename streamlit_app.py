487bed53f809769d249783e3d1b3445d95e4dc2c
Novoli8 <52935843+Novoli8@users.noreply.github.com>
2022-11-27 09:57:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if img_file and a:
     img.paste(w3,(268,336))#268,338  268,352
 
     idraw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("putches/Noto Sans Thai UI Bold_new.ttf",size=23)#23
+    font = ImageFont.truetype("putches/Noto Sans Thai UI Bold_new.ttf",size=22)#23
     idraw.text((250,336),text,(0,0,0),font=font)#250,349  , 235,336
     
     st.image(img)
